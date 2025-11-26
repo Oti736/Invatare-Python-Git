283a0b9920cb895720a5bc40d2a31a815dbd7b38
Mihai <procopiumihai2003@gmail.com>
2025-11-26 11:52:48 +0200

    Am rezolvat problema Coins ++

--- coins.py
+++ coins.py
@@ -18,18 +18,18 @@ def return_coins(coins, target_amount):
 #Testing
 coins = [1, 5, 10, 25, 100]
 amount = 10
-print(f"Target: {amount} -> {return_coins(coins,amount)}")
+print(f"Target: {amount} - {return_coins(coins,amount)}")
 
 amount = 35
-print(f"Target: {amount} -> {return_coins(coins,amount)}")
+print(f"Target: {amount} - {return_coins(coins,amount)}")
 
 amount = -10
 print(return_coins(coins,amount))
 
 amount = 0.5
-print(f"Target: {amount} -> {return_coins(coins,amount)}")
+print(f"Target: {amount} - {return_coins(coins,amount)}")
 
 print("Different coins")
 coins2 = [1, 2, 3, 5, 10, 20, 50 , 100]
 amount = 47
-print(f"Target: {amount} -> {return_coins(coins,amount)}")
+print(f"Target: {amount} - {return_coins(coins,amount)}")
